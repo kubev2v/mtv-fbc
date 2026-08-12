7d38fe77af0b17dce82da7c5174aaa18f3a80ebe
Kyl Bempah <24903083+Kyl-Bempah@users.noreply.github.com>
2026-08-12 18:12:31 -0400

    Update the semver to account for major release cutover (#261)
    * Update the semver to account for major release cutover
    
    Signed-off-by: Kyl-Bempah <24903083+Kyl-Bempah@users.noreply.github.com>
    
    * Base image registry for OCP 5 changed
    
    Signed-off-by: Kyl-Bempah <24903083+Kyl-Bempah@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kyl-Bempah <24903083+Kyl-Bempah@users.noreply.github.com>

--- opm_utils.sh
+++ opm_utils.sh
@@ -10,9 +10,21 @@ download_opm_client() {
   ./opm version
 }
 
+version_gte() {
+  local version1="$1"
+  local version2="$2"
+  
+  # Strip v prefix and compare using printf for lexicographic that works with semver
+  version1=$(echo "$version1" | sed 's/^v//')
+  version2=$(echo "$version2" | sed 's/^v//')
+  
+  printf '%s\n' "$version1" "$version2" | sort -V | head -n1 | grep -q "^${version2}$"
+}
+
+
 opm_alpha_params() {
   params=
-  if [[ ! "$1" < "v4.17" ]]; then
+  if version_gte "$1" "v4.17"; then
     params="--migrate-level=bundle-object-to-csv-metadata"
   fi
   echo "${params}"
